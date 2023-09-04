3dbde30a81c5efd0c819c34dcd11fac3fe945fb6
Alejoho <alejoreilly96@gmail.com>
2023-09-04 10:30:12 -0400

    Organized the TrakerLibrary project

--- TrackerLibrary/DataAccess/SqlConnector.cs
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.Models;
 
-namespace TrackerLibrary
+namespace TrackerLibrary.DataAccess
 {
     public class SqlConnector : IDataConnection
     {
         //TODO - Make the CreatePrize method actually save to the database
         /// <summary>
-        /// Saves a new prize to the database
+        /// Gives and ID to the new prize and save it to the database
         /// </summary>
         /// <param name="model">The prize information</param>
         /// <returns>The prize information, including the unique identifier</returns>
--- TrackerLibrary/GlobalConfig.cs
+++ TrackerLibrary/GlobalConfig.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.DataAccess;
 
 namespace TrackerLibrary
 {
     public static class GlobalConfig
     {
-        public static List<IDataConnection> Connections { get; private set; }=new List<IDataConnection>();
+        public static List<IDataConnection> Connections { get; private set; } = new List<IDataConnection>();
         public static void InitializeConnections(bool database, bool textFiles)
         {
             if(database)
@@ -21,7 +22,7 @@ namespace TrackerLibrary
             if (textFiles)
             {
                 //TODO - Create the Text Connection
-                TextConnection text = new TextConnection();
+                TextConnector text = new TextConnector();
                 Connections.Add(text);
             }
         }
