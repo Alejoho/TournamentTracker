6a1b6c23e059a96d8ace5d44361643594bee7f20
Alejoho <alejoreilly96@gmail.com>
2023-09-10 18:01:23 -0400

    Created the store procedure to insert a prize

--- scriptDB.sql
+++ scriptDB.sql
@@ -294,3 +294,29 @@ BEGIN
 	from Tournaments;
 
 END
+
+
+
+go
+-- =============================================
+-- Author:		<Alejandro>
+-- Create date: <9/10/2023>
+-- Description:	<Inserts a prize in the Prizes tables and returns the id of that record>
+-- =============================================
+CREATE PROCEDURE dbo.spPrizes_Insert
+	@PlaceNumber int,
+	@PlaceName nvarchar(50),
+	@PrizeAmount money,
+	@PrizePercentage float,
+	@id int = 0 output
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	insert into dbo.Prizes (PlaceNumber, PlaceName, PrizeAmount, PrizePercentage)
+	values (@PlaceNumber, @PlaceName, @PrizeAmount, @PrizePercentage);
+
+	select @id = SCOPE_IDENTITY();
+
+END
+GO
