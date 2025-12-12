ddcdb4c790c801df14d439ecdec920920451a424
Alejoho <142363177+Alejoho@users.noreply.github.com>
2025-12-11 21:15:03 -0500

    Create Store Procedure To Delete Tournament Data

---
+++ TournamentsDB/dbo/Stored Procedures/spTournaments_Delete.sql
@@ -0,0 +1,40 @@
+﻿CREATE PROCEDURE [dbo].[spTournaments_Delete]
+	@TournamentId int
+AS
+BEGIN
+	
+    SET NOCOUNT ON;
+
+    DELETE FROM Prizes
+    WHERE id IN (
+        SELECT PrizeId
+        FROM TournamentPrizes
+        WHERE TournamentId = @TournamentId
+    );
+
+    DELETE FROM TournamentPrizes
+    WHERE TournamentId = @TournamentId;
+
+    ------------------------------------------
+    
+    DELETE FROM TournamentEntries
+    WHERE TournamentId = @TournamentId;
+
+    ------------------------------------------
+
+    DELETE FROM MatchupEntries
+    WHERE MatchupId IN(
+        SELECT id 
+        FROM Matchups
+        WHERE TournamentId = @TournamentId);
+
+
+    DELETE FROM Matchups
+    WHERE TournamentId = @TournamentId;
+
+    ------------------------------------------
+
+    DELETE FROM Tournaments
+    WHERE id = @TournamentId;
+
+END
